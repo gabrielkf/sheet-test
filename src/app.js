b74ff0353f83768108cde1e0696f019f9651e089
gabrielkf <gabrielkf@gmail.com>
2022-03-09 13:39:32 -0300

    Replace text in spreadsheet

--- src/app.js
+++ src/app.js
@@ -1,10 +1,30 @@
-const XLSX = require('xlsx');
-const { resolve } = require('path');
+const xlsx = require('xlsx');
+const { resolve, join } = require('path');
 
-const filePath = resolve(__dirname, '..', 'assets', 'demo.xlsx');
+const fileDir = resolve(__dirname, '..', 'assets');
+const filePath = join(fileDir, 'demo.xlsx');
 
 function fromFile() {
-  // const workbook = XLSX.readFile();
+  const workbook = xlsx.readFile(filePath);
+  const plan1 = workbook.Sheets['Plan1'];
+  const plan2 = workbook.Sheets['Plan2'];
+
+  const data = xlsx.utils.sheet_to_json(plan1);
+
+  const replaced = data.map(el => {
+    if (el.modelo === '{tag}') {
+      el.modelo = 'Replacement';
+      return el;
+    }
+
+    return el;
+  });
+
+  const newBook = xlsx.utils.book_new();
+  const newSheet = xlsx.utils.json_to_sheet(replaced);
+  xlsx.utils.book_append_sheet(newBook, newSheet, 'NewData');
+
+  xlsx.writeFile(newBook, 'edited.xlsx');
 }
 
 module.exports = { fromFile };
